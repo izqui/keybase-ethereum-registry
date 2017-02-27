0b0cd544b3533c497243474554fd297902159b03
Jorge Izquierdo <jorge@izqui.me>
2017-02-27 20:46:54 +0100

    Update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const BytesHelper = artifacts.require('BytesHelper.sol')
 const KeybaseRegistry = artifacts.require('KeybaseRegistry.sol')
+const KeybaseResolver = artifacts.require('KeybaseResolver.sol')
 
 module.exports = function(deployer) {
   deployer.deploy(BytesHelper);
-  deployer.link(BytesHelper, KeybaseRegistry);
-  deployer.deploy(KeybaseRegistry, '_ropsten', false, true);
+  deployer.link(BytesHelper, [KeybaseRegistry, KeybaseResolver]);
+  deployer.deploy(KeybaseRegistry, '_ropsten', false);
+  deployer.deploy(KeybaseResolver, '0x1234', '_ropsten', false);
 };
