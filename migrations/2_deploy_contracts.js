81401b918056a391839c732f63d3b18d8edee1bb
Jorge Izquierdo <jorge@izqui.me>
2017-03-08 16:56:59 +0100

    Back to legacy addrs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,6 @@ const KeybaseResolver = artifacts.require('KeybaseResolver.sol')
 module.exports = function(deployer) {
   deployer.deploy(BytesHelper);
   deployer.link(BytesHelper, [KeybaseRegistry, KeybaseResolver]);
-  deployer.deploy(KeybaseRegistry, '_kovan', false);
-  deployer.deploy(KeybaseResolver, '0x1234', '_kovan', false);
+  deployer.deploy(KeybaseRegistry, '_kovan', true);
+  deployer.deploy(KeybaseResolver, '0x1234', '_kovan', true);
 };
